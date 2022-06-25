6a56e35693b87fb2164171c779547490a59187c3
abhishek358 <56888353+abhishek358@users.noreply.github.com>
2022-06-25 13:28:25 +0530

    Add files via upload

---
+++ smartcontactmanager/src/main/java/com/smart/smartcontactmanager/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.smart.smartcontactmanager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    
+    @RequestMapping("/")
+    public String home(Model model)
+    {
+        model.addAttribute("title","Home - Smart Contact Manager");
+        return "home";
+    }
+
+    @RequestMapping("/about")
+    public String about(Model model)
+    {
+        model.addAttribute("title","About - Smart Contact Manager");
+        return "about";
+    }
+
+
+
+}
+
